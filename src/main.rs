98755f822736f550ec097399b580f197966be783
Daniil Naumetc <daniil.naumetc@gmail.com>
2021-02-03 20:48:24 +0100

    xx

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::cell::{RefCell};
+use std::cell::RefCell;
+use std::rc::Rc;
 
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct Reservation {
     weight: i32
 }
 
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct Unit {
     capacity: i32
 }
@@ -25,11 +28,11 @@ impl Sameable for Unit {
 }
 
 trait Fittable<T> {
-    fn fits_in(&self, container: T) -> bool;
+    fn fits_in(&self, container: &T) -> bool;
 }
 
 impl Fittable<Unit> for Reservation {
-    fn fits_in(&self, container: Unit) -> bool {
+    fn fits_in(&self, container: &Unit) -> bool {
         self.weight <= container.capacity
     }
 }
@@ -44,10 +47,12 @@ impl RoundRobin {
         Self { max: max, cursor: RefCell::new(None)}
     }
 
-    fn next(&self) -> Option<usize> {
+    fn next(&self) -> bool {
+        let mut turned = false;
         let new_cursor = if let Some(mut cursor) = *self.cursor.borrow() {
             cursor += 1;
             if cursor >= self.max {
+                turned = true;
                 None
             } else {
                 Some(cursor)
@@ -56,12 +61,14 @@ impl RoundRobin {
             Some(0)
         };
         *self.cursor.borrow_mut() = new_cursor.clone();
-        new_cursor
+        turned
     }
 
-    fn jump(&self, to: Option<usize>) -> Option<usize> {
+    fn jump(&self, to: Option<usize>) -> bool {
+        let mut turned = false;
         let new_cursor = if let Some(to) = to {
             if to >= self.max {
+                turned = true;
                 None
             } else {
                 Some(to)
@@ -71,23 +78,25 @@ impl RoundRobin {
         };
 
         *self.cursor.borrow_mut() = new_cursor.clone();
-        new_cursor
+        turned
     }
 
-    fn skip(&self, hops: usize) -> Option<usize> {
+    fn skip(&self, hops: usize) -> bool {
+        let mut turned = false;
         let new_cursor = if let Some(cursor) = *self.cursor.borrow() {
             cursor as i32 + hops as i32
         } else {
             hops as i32 - 1
         } as usize;
         let new_cursor = if new_cursor >= self.max {
+            turned = true;
             None
         } else {
             Some(new_cursor)
         };
 
         *self.cursor.borrow_mut() = new_cursor.clone();
-        new_cursor
+        turned
     }
 
     fn state(&self) -> Option<usize> {
@@ -101,14 +110,45 @@ impl RoundRobin {
 //xxxxxxx
 // y  yyy
 //    y
-struct Solver2<A: Sameable + Fittable<B>, B: Sameable> {
+struct Solver2<A: Sameable + Fittable<B> + Eq + Ord + PartialEq + PartialOrd, B: Sameable + Eq + Ord + PartialEq + PartialOrd> {
     xs: Vec<A>,
-    ys: Vec<B>,
+    ys: Vec<Rc<B>>,
+    cursor: usize,
+    max: usize,
+    y_links: Vec<Vec<Rc<B>>>,
+    robins: Vec<RoundRobin>, 
 }
 
-impl<A: Sameable + Fittable<B>, B: Sameable> Solver2<A, B> {
-    fn new(xs: Vec<A>, ys: Vec<B>) -> Self {
-        Self {xs: xs, ys: ys}
+impl<A: Sameable + Fittable<B> + Eq + Ord + PartialEq + PartialOrd, B: Sameable + Eq + Ord + PartialEq + PartialOrd> Solver2<A, B> {
+    fn new(mut xs: Vec<A>, mut ys: Vec<B>) -> Self {  
+        let max = xs.len();
+        xs.sort();
+        ys.sort();
+
+        let ys = ys.into_iter().map(|y|Rc::new(y)).collect::<Vec<Rc<B>>>();
+
+        // let (y_links, robins) = Self::produce_links_and_robins(&xs, &ys);
+
+        Self {xs: xs, ys: ys, cursor: 0, max: max, y_links: vec![], robins: vec![]}
+    }
+
+    fn fill_links_and_robins(&mut self) {
+        for x in self.xs.iter() {
+            let mut links = vec![];
+            for y in self.ys.iter(){
+                if x.fits_in(y) {
+                    links.push(y.clone())
+                }
+            }
+            self.robins.push(RoundRobin::new(links.len()));
+            self.y_links.push(links);
+        }
+    }
+
+
+    // Gets current y(room) at ith x(reservation) link. Used to find whether current y at current x 
+    fn get_y_at_ith_x(&self, i: usize) {
+
     }
 
 
@@ -119,7 +159,8 @@ fn main() {
 
     let units = vec![Unit{capacity: 2}, Unit{capacity: 3}, Unit{capacity: 3}, Unit{capacity: 4}];
     let reservations = vec![Reservation{weight: 2}, Reservation{weight: 3}, Reservation{weight: 3}, Reservation{weight: 4}];
-    let solver = Solver2::new(reservations, units);
+    let mut solver = Solver2::new(reservations, units);
+    solver.fill_links_and_robins();
 
     let rr = RoundRobin::new(3);
     // rr.next();
